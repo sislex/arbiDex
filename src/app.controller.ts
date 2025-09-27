a2b0860307a2fcff4ae9f1748375614617bd452b
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-09-27 23:36:12 +0300

    refactor: remove unused QuotesService from AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,15 +3,12 @@ import { TokensService } from './db/services/tokens/tokens.service';
 import { Tokens } from './db/entities/Tokens';
 import { Dexes } from './db/entities/Dexes';
 import { DexesService } from './db/services/dexes/dexes.service';
-import { QuotesService } from './db/services/quotes/quotes.service';
-import { Quotes } from './db/entities/Quotes';
 
 @Controller('api')
 export class AppController {
   constructor(
     private readonly tokensService: TokensService,
     private readonly dexesService: DexesService,
-    private readonly quotesService: QuotesService,
   ) {}
 
   @Get('getAllTokens')
