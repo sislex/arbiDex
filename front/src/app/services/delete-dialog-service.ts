e0ca64fc521b034b3d14d71d4ba391d3889ef0ea
NikolayTola <kolyatola@mail.ru>
2025-12-18 00:07:06 +0300

    work all deleted API but need fix entities

---
+++ front/src/app/services/delete-dialog-service.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { ConfirmationPopUpContainer } from '../containers/confirmation-pop-up-container/confirmation-pop-up-container';
+import { MatDialog } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DeleteDialogService {
+  private dialog = inject(MatDialog);
+
+  openDelete(row: any, title: string) {
+    return this.dialog.open(ConfirmationPopUpContainer, {
+      width: '400px',
+      height: '300px',
+      data: {
+        title: `Delete ${title}`,
+        message: `Are you sure you want to delete "${row?.tokenName}"?`,
+        buttons: ['yes', 'no']
+      }
+    }).afterClosed();
+  }
+}
