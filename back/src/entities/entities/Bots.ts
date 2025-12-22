5679787f0782e038908d9d16125160591dc836c8
NikolayTola <kolyatola@mail.ru>
2025-12-22 09:16:40 +0300

    add new entities

---
+++ back/src/entities/entities/Bots.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Servers } from './Servers';
+import { JobBotRelations } from './JobBotRelations';
+
+@Index('bots_pkey', ['botId'], { unique: true })
+@Entity('bots', { schema: 'public' })
+export class Bots {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'bot_id' })
+  botId: string;
+
+  @Column('character varying', {
+    name: 'bot_name',
+    nullable: true,
+    length: 255,
+  })
+  botName: string | null;
+
+  @Column('character varying', {
+    name: 'description',
+    nullable: true,
+    length: 255,
+  })
+  description: string | null;
+
+  @ManyToOne(() => Servers, (servers) => servers.bots, { onDelete: 'RESTRICT' })
+  @JoinColumn([{ name: 'server_id', referencedColumnName: 'serverId' }])
+  server: Servers;
+
+  @OneToMany(() => JobBotRelations, (jobBotRelations) => jobBotRelations.bot)
+  jobBotRelations: JobBotRelations[];
+}
