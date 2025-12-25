f2dd5b858a32327da7e13ec4ce8d785ff82aa140
NikolayTola <kolyatola@mail.ru>
2025-12-25 11:19:45 +0300

    added checkBox in ag-grid-component

--- front/src/app/components/ag-grid/ag-grid.ts
+++ front/src/app/components/ag-grid/ag-grid.ts
@@ -16,12 +16,20 @@ export class AgGrid {
   @Input() rowData: any[] = [];
   @Input() colDefs: ColDef[] = [];
   @Input() defaultColDef: ColDef = {};
+  @Input() isRowSelection: any = undefined;
 
   @Output() emitter = new EventEmitter();
 
   onRowDoubleClicked($event: any) {
     this.emitter.emit({
       event: 'AgGrid:DOUBLE_CLICKED_ROW',
+      row: $event
+    });
+  }
+
+  onSelectionChanged($event: any) {
+    this.emitter.emit({
+      event: 'AgGrid:SET_CHECKBOX_ROW',
       row: $event
     });
   }
