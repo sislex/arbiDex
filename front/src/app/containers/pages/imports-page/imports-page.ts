1d94ba6a952709bc5ce25a5d41b3bbcc3db1f661
Nikolay <kolyatola@mail.ru>
2026-01-20 14:51:23 +0300

    get reserves func (viem lib)

--- front/src/app/containers/pages/imports-page/imports-page.ts
+++ front/src/app/containers/pages/imports-page/imports-page.ts
@@ -2,6 +2,9 @@ import {Component, inject, Input} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {poolImportCamelotV3, setFee, tokenImportCamelotV3} from '../../../+state/main/main.actions';
 import {Store} from '@ngrx/store';
+import {Web3Service} from '../../../services/web3-service';
+import {getPoolsDataResponse} from '../../../+state/db-config/db-config.selectors';
+import {from, switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-imports-page',
@@ -16,14 +19,27 @@ export class ImportsPage {
   @Input() data: string = '';
 
   private store = inject(Store)
+  private web3Service = inject(Web3Service)
 
-  events(note: string) {
+  async events(note: string) {
     if (note === 'token') {
       this.store.dispatch(tokenImportCamelotV3({data: this.data}));
     } else if (note === 'pool') {
       this.store.dispatch(poolImportCamelotV3({data: this.data}));
     } else if (note === 'setfee') {
       this.store.dispatch(setFee());
+    } else if (note === 'getReserves') {
+      const poolsWithReserves$ = this.store.select(getPoolsDataResponse).pipe(
+        switchMap(pools => {
+          const addresses = pools.map(p => p.poolAddress as `0x${string}`);
+
+          return from(this.web3Service.getAllPoolsData(addresses));
+        })
+      );
+
+      poolsWithReserves$.subscribe(reserves => {
+        console.log('Резервы получены:', reserves);
+      });
 
     }
   }
