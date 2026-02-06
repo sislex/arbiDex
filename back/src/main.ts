b1587531618f0fa49e352b8f5cc32bdb2c0cdfad
Nikolay <kolyatola@mail.ru>
2026-02-06 12:25:59 +0300

    local settings

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://45.135.182.251:4203',
+    origin: '*',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
