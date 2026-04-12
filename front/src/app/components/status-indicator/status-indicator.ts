ba0a968529e1874f773a2737991fdf3022e28f01
Nikolay <kolyatola@mail.ru>
2026-04-12 15:00:25 +0300

    test

---
+++ front/src/app/components/status-indicator/status-indicator.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, Observable, of} from 'rxjs';
+import { ICellRendererParams } from 'ag-grid-community';
+import {AsyncPipe} from '@angular/common';
+import {startWith} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-status-indicator',
+  imports: [
+    AsyncPipe
+  ],
+  templateUrl: './status-indicator.html',
+  styleUrl: './status-indicator.scss',
+})
+export class StatusIndicator implements ICellRendererAngularComp {
+  private http = inject(HttpClient);
+  status$!: Observable<string>;
+
+  agInit(params: ICellRendererParams): void {
+    const { ip, port } = params.data || {};
+
+    if (!ip || !port) {
+      this.status$ = of('offline');
+      return;
+    }
+
+    // Формируем URL согласно вашему шаблону
+    const url = `http://45.135.182.251:8230/server/${ip}:${port}`;
+
+    this.status$ = this.http.get(url, { observe: 'response' }).pipe(
+      map(res => {
+        // Если сервер отвечает 200-299, считаем что он Online
+        return res.ok ? 'online' : 'offline';
+      }),
+      catchError(() => of('offline')),
+      startWith('loading')
+    );
+  }
+
+
+  refresh(params: ICellRendererParams): boolean {
+    return false;
+  }
+}
